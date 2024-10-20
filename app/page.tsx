570afd9d56b65c721388089b695d6094f539127e
khawaja naqeeb <shajiekns@yahoo.com>
2024-10-20 23:32:05 +0500

    profile picture addt

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,56 @@
 // app/page.tsx
-import Link from 'next/link';
-import './globals.css'; // Import your global CSS styles
+import Link from "next/link";
+import "./globals.css"; 
+import profile from "./public/n2.jpg";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <header>
-        
-        <h1>Khawaja Naqeeb Uddin</h1>
-        <p> Ambitious  Web Developer</p>
-        <nav>
-          <ul>
-            <li><Link href="#about">About</Link></li>
-            <li><Link href="#skills">Skills</Link></li>
-            <li><Link href="#education">Education</Link></li>
-            <li><Link href="#experience">Experience</Link></li>
-            <li><Link href="#contact">Contact</Link></li>
-          </ul>
-        </nav>
+      <div className="pic">
+          <Image
+            src={profile}
+            width={150}
+            height={150}
+            className="pic"
+            alt="Picture of the author"
+            
+          />
+        </div>
+        <div className="head">
+          <h1>Khawaja Naqeeb Uddin</h1>
+          <p> Ambitious Web Developer</p>
+          <nav>
+            <ul>
+              <li>
+                <Link href="#about">About</Link>
+              </li>
+              <li>
+                <Link href="#skills">Skills</Link>
+              </li>
+              <li>
+                <Link href="#education">Education</Link>
+              </li>
+              <li>
+                <Link href="#experience">Experience</Link>
+              </li>
+              <li>
+                <Link href="#contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
 
       <main>
         <section id="about">
           <h2>About Me</h2>
-          <p>I'm an ambitious web developer eager to learn new things with a passion for building atractive,functional and user-friendly websites.</p>
+          <p>
+            I'm an ambitious web developer eager to learn new things with a
+            passion for building atractive,functional and user-friendly
+            websites.
+          </p>
         </section>
 
         <section id="skills">
@@ -48,16 +75,35 @@ export default function Home() {
 
         <section id="experience">
           <h2>Experience</h2>
-          <p>Worked as Computer teacher for class 9th and 10th, taught basics of C++,HTML,CSS.</p>
-          <p>Worked in a computer sales and repair company named Computer Outlet , where performed hardware and software installation.</p>
+          <p>
+            Worked as Computer teacher for class 9th and 10th, taught basics of
+            C++,HTML,CSS.
+          </p>
+          <p>
+            Worked in a computer sales and repair company named Computer Outlet
+            , where performed hardware and software installation.
+          </p>
         </section>
 
         <section id="contact">
           <h2>Contact</h2>
           <p>Email: naqeebkns@gmail.com</p>
           <p>Phone: +923452435349</p>
-          <p>LinkedIn: <a href="https://www.linkedin.com/in/naqeeb-kns-75203a143/" target="_blank">LinkedIn</a></p>
-          <p>GitHub: <a href="https://github.com/khawajanaqeeb" target="_blank">GitHub</a></p>
+          <p>
+            LinkedIn:{" "}
+            <a
+              href="https://www.linkedin.com/in/naqeeb-kns-75203a143/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            GitHub:{" "}
+            <a href="https://github.com/khawajanaqeeb" target="_blank">
+              GitHub
+            </a>
+          </p>
         </section>
       </main>
 
